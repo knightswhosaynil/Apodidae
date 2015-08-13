4459b61c4c8cb3c5e530b4932ec65932d246d867
Raymond, Johnathan D <johnathan.d.raymond@intel.com>
2015-08-13 14:30:29 -0700

    Initial Commit

---
+++ Apodidae/Apodidae.h
@@ -0,0 +1,19 @@
+//
+//  Apodidae.h
+//  Apodidae
+//
+//  Created by Johnathan D Raymond on 8/13/15.
+//  Copyright © 2015 Knights Who Say Nil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Apodidae.
+FOUNDATION_EXPORT double ApodidaeVersionNumber;
+
+//! Project version string for Apodidae.
+FOUNDATION_EXPORT const unsigned char ApodidaeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Apodidae/PublicHeader.h>
+
+
